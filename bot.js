369a0fb4d9422a5e115e8ceed3dfcdbd73403453
Tiger Li <22798884+tigerpway@users.noreply.github.com>
2019-10-23 19:20:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var botID = process.env.BOT_ID;
 function respond() {
       var request = JSON.parse(this.req.chunks[0]),
       //botRegex = /^\/lx$/;
-        botRegex = /lx/;  
+        botRegex = /lx/i;  
 
   if(request.text && botRegex.test(request.text)) {  
     this.res.writeHead(200);
